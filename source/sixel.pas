bde77f3a195de1b0e9b6730b1798222284e3d782
Haitham Shatti <haitham.shatti@gmail.com>
2025-07-02 11:45:59 +0200

    sixel + dither

--- source/sixel.pas
+++ source/sixel.pas
@@ -2,8 +2,7 @@ unit sixel;
 {$ifdef FPC}
   {$mode Delphi}
   {$modeswitch nestedprocvars}
-{$else}
-  {$M 16384, $800000}
+  {$ModeSwitch advancedrecords}
 {$endif}
 {$pointermath on}
 
@@ -12,7 +11,15 @@ unit sixel;
 interface
 
 uses
-  Classes, SysUtils, Math, steroids, termesc, nTensors;
+  Classes, SysUtils, Math, Generics.Collections, windows, SortedMap, termesc, nTensors
+  {$ifdef USE_MULTITHREADING}
+  , steroids
+  {$endif}
+  ;
+
+
+
+procedure printSixel(const buf:Pointer; const width, height:SizeInt; const Dither:boolean = false);
 
 implementation
 
@@ -113,112 +120,373 @@ end;
 
 type
 
+  { TSixel }
+
   TSixel = packed record
-    r,g,b:byte
+    r,g,b:byte;
+    class operator add      (const a, b:TSixel):TSixel;
+    class operator subtract (const a, b:TSixel):TSixel;
+    class operator multiply (const a, b:TSixel):TSixel;
+    class operator IntDivide(const a, b:TSixel):TSixel;
+    class operator multiply (const a:TSixel; const b:byte):TSixel;
+    class operator IntDivide(const a:TSixel; const b:byte):TSixel;
+    class operator multiply (const a:TSixel; const b:single):TSixel;
+
+
+  end;
+  TSixelS = packed record
+    r,g,b:single
   end;
   PSixel = ^TSixel;
 
-function col2Index(const pxl:TSixel):longword; inline;
+  TColorMap = TDictionary<TSixel, longint>;
+
+function comp(const a, b:longword):SizeInt; winapi;
 begin
-  result := trunc(100*pxl.r / 255) + 100*trunc(100*pxl.g / 255) + 10000*trunc(100*pxl.b / 255)
+  result := a-b
 end;
 
-function  index2Col(const pxl:longword):TSixel; inline;
+function sxlIsEqual(const a, b: TSixel):boolean;
 begin
-  result.r := pxl mod 100;
-  result.g := (pxl div 100) mod 100;
-  result.b := pxl div 10000
+  result := (a.r=b.r) and (a.g=b.g) and (a.b=b.b)
 end;
 
-procedure printSixel(const buf:PByte; const width, height:SizeInt);
-type
-  TColorPal = array[0..1010100 -1] of longword;
-  utils = TTools<longword>;
+function intToAnsiStr(const int:integer):ansistring;   inline;
+begin
+  result := sysutils.intToStr(int)
+end;
+
+procedure printSixel(const buf: Pointer; const width, height: SizeInt; const Dither: boolean);
+const pixel = 3;
+     pixcount = 6 div pixel;
+     COLOR_SPACE : array[0..2] of byte = (8, 8, 4);
 var
-  P : PSixel absolute buf;
+  P                      : PSixel;
 //  bitPix  : array of byte;
-  bitPix : byte;
-  idx, i, x, y: SizeInt;
-  charCount : longword;
-  colorPal : TColorPal;
-  c, lastC : longint;
-  //s,
-  plt : ansistring;
-  clrCount, reg : longInt;
-  sxl :TSixel;
+  pos                    : SizeInt;
+  colorPal               : TColorMap;
+  plt                    : ansistring;
+  sxlMin, sxlMax         : TSixel;
+  sxlScaler              : TSixel;
+
+  function getSixel(const y, x :SizeInt)    : TSixel;
+  begin
+    result := p[y*width + x]
+  end;
+
+  procedure encodeNum(const ctl : ansichar; const num:longint);
+  var sz : SizeInt;
+  begin
+    if length(plt)<pos + 6 then
+      setLength(plt, 2*(pos + 5));
+    plt[pos] := ctl; inc(pos);
+    if num>99 then sz := 3 else if num>9 then sz:=2 else sz := 1;
+    move(intToAnsistr(num)[1], plt[pos], sz); inc(pos,sz);
+  end;
+  procedure encodeChar(const ctl:ansichar; const num:longint; const str :ansichar);
+  var sz : SizeInt;
+  begin
+    if length(plt)<pos + 7 then
+      setLength(plt, 2*(pos + 6));
+    plt[pos] := ctl; inc(pos);
+    if num>99 then sz := 3 else if num>9 then sz:=2 else sz := 1;
+    move(intToAnsistr(num)[1], plt[pos], sz); inc(pos,sz);
+    plt[pos] := str; inc(pos);
+  end;
+
+  procedure encodeStr(const str:ansistring);
+  var sz :SizeInt;
+  begin
+    sz := length(str);
+    if length(plt) <pos + sz then
+      setLength(plt, 2*(pos + sz));
+    move(str[1], plt[pos], sz); inc(pos, sz)
+  end;
+
+  procedure encodeRepeat(const num:longint; const str:ansichar); inline;
+  var i: longint;
+  begin
+    for i:= 0 to num div 255 -1 do
+      encodeChar('!', 255, str);
+    if num mod 255>0 then
+      encodeChar('!', num mod 255, str)
+  end;
+
+  function clr100(const col:TSixel):TSixel; inline;
+  begin
+    result.r := trunc(100*col.r/255);
+    result.g := trunc(100*col.g/255);
+    result.b := trunc(100*col.b/255);
+  end;
+
+  function clrSpc(const col:TSixel):TSixel;
+  begin
+    //result.r := round(100*(col.r - sxlMin.r)/255) div sxlScaler.r;
+    //result.g := round(100*(col.g - sxlMin.g)/255) div sxlScaler.g;
+    //result.b := round(100*(col.b - sxlMin.b)/255) div sxlScaler.b;
+    //result.r := sxlScaler.r*(round(100*col.r/255) div sxlScaler.r);
+    //result.g := sxlScaler.g*(round(100*col.g/255) div sxlScaler.g);
+    //result.b := sxlScaler.b*(round(100*col.b/255) div sxlScaler.b);
+    result := sxlScaler * (col div sxlScaler);
+  end;
+
+  procedure reCalcPalette(var mi, ma:TSixel; var s:TSixel);
+  var i, r, g, b:longint; sx :TSixel;
+  begin
+    //clrCount := 0;
+    colorPal.Clear;
+    mi.r := p[0].r;
+    mi.g := p[0].g;
+    mi.b := p[0].b;
+    ma.r := p[0].r;
+    ma.g := p[0].g;
+    ma.b := p[0].b;
+    for i:=1 to height*width-1 do begin
+      sx := p[i];
+      mi.r := Math.min(sx.r, mi.r);
+      mi.g := Math.min(sx.g, mi.g);
+      mi.b := Math.min(sx.b, mi.b);
+      ma.r := Math.max(sx.r, ma.r);
+      ma.g := Math.max(sx.g, ma.g);
+      ma.b := Math.max(sx.b, ma.b);
+    end;
+    //s.r := round(100 *(ma.r-mi.r) / 255) div 5;     // scaler
+    //s.g := round(100 *(ma.g-mi.g) / 255) div 5;
+    //s.b := round(100 *(ma.b-mi.b) / 255) div 5;
+    s.r := 100 div (COLOR_SPACE[0]-1);
+    s.g := 100 div (COLOR_SPACE[1]-1);
+    s.b := 100 div (COLOR_SPACE[2]-1);
+    i:=16;
+
+    for r:=0 to COLOR_SPACE[0]-1 do
+      for g :=0 to COLOR_SPACE[1]-1 do
+        for b :=0 to COLOR_SPACE[2]-1 do begin
+          sx.r := s.r*r;
+          sx.g := s.g*g;
+          sx.b := s.b*b;
+          encodeNum('#', i);
+          encodeNum(';', 2);
+          //encodeNum(';', mi.r + s.r*r);
+          //encodeNum(';', mi.g + s.g*g);
+          //encodeNum(';', mi.b + s.b*b); // encodeStr(#13#10);
+          encodeNum(';', s.r*r);
+          encodeNum(';', s.g*g);
+          encodeNum(';', s.b*b); // encodeStr(#13#10);
+          colorPal.Add(sx, i);
+          inc(i)
+        end;
+  end;
+
+const           // enable SIXEL + START SIXEL
+  START_SIXEL = #$1B']80h'      + #$1B'Pq';
+  END_SIXEL   = #$1B'\';  // ST
+var
+  charCount              : longword;
+  bitPix                 : byte;
+  sxl, lastSxl, q_err    : TSixel;
+  p1, p2, p3, p4         : PSixel;
+  dith                   : TArray<TSixel>;
+  //clrCount,
+  reg                    : longInt;
+  i, idx, x, y           : SizeInt;
+  t:int64;
+label done;
 begin
-  for i:=0 to high(colorPal) do
-    colorPal[i] := $ffffff;
-  clrCount := 0;
-  for i:=0 to height*width-1 do begin
-    c := col2Index(p[i]);
-    //sxl:=index2Col(c);
-    if colorPal[c]=$ffffff then inc(clrCount);
-    colorPal[c]:= c;
-  end;
-  utils.QuickSort(@colorPal[0], 0, high(colorPal));
-  plt := '';
-  for i:=0 to clrCount-1 do begin
-    sxl := index2Col(colorPal[i]);
-    plt := plt + '#'+intToStr(i) + ';2;'+intToStr(sxl.r) +';'+intToStr(sxl.g)+';'+intToStr(sxl.b);
-    //plt := plt + format('#%d;2;%d;%d;%d',[i, sxl.r, sxl.g, sxl.b])
-  end;
+
+  colorPal := TColormap.Create(width* height);
+  setLength(plt, width*height);
+  setLength(dith, width*height);
+  pos:=1;
+  //move(buf^, dith[0], width*height*sizeOf(p[0]));
+  p := pointer(buf);
+  reCalcPalette(sxlMin, sxlMax, sxlScaler);
+
+  for i:=0 to high(dith) do dith[i] := clr100(p[i]);
+  p := pointer(dith);
+
+  if Dither then
+    for y:=0 to height -2 do
+      for x:= 1 to width -2 do begin
+        idx                  := y*width + x;
+        lastSxl              := dith[idx];
+        sxl                  := clrSpc(lastSxl);
+        dith[idx]            := sxl;
+        q_err                := lastSxl - sxl;
+        p1 := @p[idx         +1];
+        p2 := @p[idx + width -1];
+        p3 := @p[idx + width   ];
+        p4 := @p[idx + width +1];
+
+        p1^ := p1^ + q_err * (7/16);
+        p2^ := p2^ + q_err * (3/16);
+        p3^ := p3^ + q_err * (5/16);
+        p4^ := p4^ + q_err * (1/16);
+      end;
+
 //  setLength(bitPix, width);
+  //write(copy(plt,1, pos-1));
+
   charCount:=0;
-  for y := 0 to Math.ceil(height/6) do begin
-    for i:= 0 to 5 do begin
-      if y*6 + i >= height then break;
-      lastC :=-1;
+
+  //t:=GetTickCount64;
+  for y := 0 to Math.ceil(height/pixCount) do begin
+    for i:= 0 to pixCount-1 do begin
+      if y*pixCount + i >= height then break;
+      //lastC := $ffffff;
+      lastSxl.r := $ff;
       for x:=0 to width-1 do begin
-        idx := y*6*width + i*width + x;
-        c := col2Index(p[idx]);
-        bitPix := 63 + (1 shl i);
-        if c=lastC then begin
-          //plt := plt + ansichar(bitPix[x]);
+        //idx := getSixel(y*pixCount + i, x);
+        //c := col2Index(p[idx]);
+        //sxl := col100(getSixel(y*pixCount + i, x));
+        //sxl := col2Six(sxl);
+        sxl := clrSpc(getSixel(y*pixCount + i, x));
+        bitPix := 63 + ((1 shl pixel)-1) shl (i*pixel);
+        if sxlIsEqual(sxl, lastSxl) then begin
+        //if c=lastC then begin
           inc(charCount)
         end
         else
         begin
           if charCount>0 then begin
-            plt := plt + '!' +intToStr(charCount)+ ansichar(bitPix);
+            encodeRepeat(charCount*pixel, ansichar(bitPix));
+            //encodeChar('!', charCount*pixel, ansichar(bitPix));
             charCount:=0;
           end;
-          reg := utils.BinSearch(@colorPal[0], c, clrCount);
-          assert(reg>=0, '[printSixel] Cannot fine color in palettte!');
-          plt := plt + '#'+ intToStr(reg) + ansichar(bitPix);
+          //colorPal.TryGetValue(c, reg);
+          if not colorPal.TryGetValue(sxl, reg) then
+            raise Exception.Create('Cannot locate color in the palette!');
+          encodeChar('#', reg, ansichar(bitPix));
+          charCount:=0;
+          if pixel>1 then
+            fillchar(plt[pos], pixel-1, bitPix); inc(pos, pixel-1);
         end;
-        lastC := c;
+        lastSxl := sxl;
+        //lastC := c
       end;
       if charCount>0 then begin
-        plt := plt + '!' +intToStr(charCount)+ ansichar(bitPix);
+        encodeRepeat(charCount*pixel, ansichar(bitPix));
+        //encodeChar('!', charCount*pixel, ansichar(bitPix));
         charCount:=0;
       end;
-      if i=5 then
-        plt := plt +'-'
-      else
-        plt := plt +'$'
+      plt[pos]:='$'; inc(pos);   // overlay line
     end;
-    //plt := plt + '-'
+    plt[pos]:='-'; inc(pos)  // draw in new line
   end;
-  plt := plt +'-' ;
-  plt := #$1B'Pq'+
-         '"1;1;'+intToStr(width)+';'+intTostr(height)+plt +
-         #$1B'\' ;
-  {$ifdef _MSWiNDOWS}
-  WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), @plt[1], length(plt), charCount, nil);
-  {$else}
-  write(plt)
-  {$endif}
+done:
+  freeAndNil(colorPal);
+  //writeln((GetTickCount64-t)/1000:1:3,'ms');
+
+(*
+     P1          Pixel Aspect Ratio
+                 (Vertical:Horizontal)
+
+     Omitted     2:1
+     0 or 1      5:1
+     2           3:1
+     3 or 4      2:1
+     5 or 6      2:1
+     7,8, or 9   1:1
+
+    P2 selects how the terminal draws the background color.   You can 3$
+    use one of three values.
+
+         P2          Meaning
+
+         0 or 2      Pixel positions  specified as  0 are  set to the
+                     current background color.
+
+         1           Pixel positions specified as  0 remain  at their
+                     current color.
+
+    P3 is  the horizontal  grid size  parameter.  The horizontal grid
+    size is the horizontal distance  between  two  pixel  dots.   The
+    VT300 ignores  this parameter because the horizontal grid size is
+    fixed at 0.0195 cm (0.0075 in).
+*)
+
+  setLength(plt, pos-1);
+  write(START_SIXEL);
+  write('"1;1;'+intToAnsistr(width)+';'+intToAnsistr(height));
+  write(plt);
+  write(END_SIXEL);
+  //write(plt)
 end;
 
-//var
-//  a : TArray<byte>;
-//
-//initialization
-//  setLength(a, 800*800*3);
-//  Mandel(pointer(a), 800, 800);
-//  printSixel(pointer(a), 800, 800);
-//  readln
+
+var
+  bt : TByteTensor;
+  t:int64;
+
+{ TSixel }
+
+class operator TSixel.add(const a, b: TSixel): TSixel;
+begin
+  result.r := a.r + b.r;
+  result.g := a.g + b.g;
+  result.b := a.b + b.b;
+end;
+
+class operator TSixel.subtract(const a, b: TSixel): TSixel;
+begin
+  result.r := a.r - b.r;
+  result.g := a.g - b.g;
+  result.b := a.b - b.b;
+end;
+
+class operator TSixel.multiply(const a, b: TSixel): TSixel;
+begin
+  result.r := a.r * b.r;
+  result.g := a.g * b.g;
+  result.b := a.b * b.b;
+end;
+
+class operator TSixel.IntDivide(const a, b: TSixel): TSixel;
+begin
+  result.r := a.r div b.r;
+  result.g := a.g div b.g;
+  result.b := a.b div b.b;
+end;
+
+class operator TSixel.multiply(const a: TSixel; const b: byte): TSixel;
+begin
+  result.r := a.r * b;
+  result.g := a.g * b;
+  result.b := a.b * b;
+end;
+
+class operator TSixel.IntDivide(const a: TSixel; const b: byte): TSixel;
+begin
+  result.r := a.r div b;
+  result.g := a.g div b;
+  result.b := a.b div b;
+end;
+
+class operator TSixel.multiply(const a: TSixel; const b: single): TSixel;
+begin
+  result.r := trunc(a.r * b);
+  result.g := trunc(a.g * b);
+  result.b := trunc(a.b * b);
+end;
+
+initialization
+  //{$ifdef FPC}
+  //bt.loadFromImage('dog.jpg');
+  //{$else}
+  //bt.loadFromImage('../../../../FPC/ConsoleTest/dog.jpg');
+  //{$endif}
+  ////bt.print(0.2);
+  //bt := bt.Permute([1, 2, 0]);
+  ////bt.resize([400, 600, 3]);
+  ////Mandel(bt.Data, bt.h(), bt.c());
+  ////bt := bt.Permute([1, 2 , 0]);
+  ////bt.SaveToImage('mandel.jpg');
+  //repeat
+  //  cursorHome();
+  //  t := getTickCount64;
+  //  printSixel(bt.data, bt.h, bt.c);
+  //  write((GetTickCount64-t)/1000:1:3,'s');
+  //  readln
+  //until false;
 
 end.
 
